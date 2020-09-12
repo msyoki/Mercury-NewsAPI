1e9d9c6f49286288a2734c734ffd89c0842c2bb9
Msyoki <msyokimutua@gmail.com>
2020-09-12 15:09:49 +0300

    add title text update

--- app/views.py
+++ app/views.py
@@ -2,17 +2,17 @@ from flask import render_template
 from app import app
 
 #views 
-@app.route('/')
+@app.route('/')  
 def index():
     '''
     View root page function that returns the index page and its data
     '''
-    title = 'NEWS API'
+    title = 'Home - Welcome to NEWS API'
     statement = '"Never miss out on the highlights"'
     return render_template('index.html',title = title,statement = statement)
 
 
-@app.route('/topic/<topic_id>')
+@app.route('/topic/<topic_id>')  
 def topic(topic_id):
     '''
     View topic page fuction that returns the topic details page and its data 
