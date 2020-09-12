6bd35409cc3203c38a5775393c1bb43b71e1ae4f
Msyoki <msyokimutua@gmail.com>
2020-09-13 02:06:27 +0300

    edit index function

--- app/views.py
+++ app/views.py
@@ -1,20 +1,23 @@
 from flask import render_template
 from app import app
-
+from .request import get_sources
 #views 
-@app.route('/')  
+@app.route('/')
 def index():
-    '''
-    View root page function that returns the index page and its data
-    '''
-    title = 'Home - Welcome to NEWS API'
-    statement = '"Never miss out on the highlights"'
-    return render_template('index.html',title = title,statement = statement)
+    """
+    View root page function that returns index page and the various news sources
+    """
+    title = "Home- Welcome to the News Highlights Website"
+   
+    # Getting the news sources
+    news_sources = get_sources('general')
+    tag_line = '"Never miss on the headlines"'
+    return render_template('index.html', title=title, sources=news_sources ,statement = tag_line)
 
 
-@app.route('/article/<int:source_id>')  
-def topic(source_id):
-    '''
-    View topic page fuction that returns the topic details page and its data 
-    '''
-    return render_template('topic.html',id = source_id)
+# @app.route('/source/<source_id>')  
+# def source(source_id):
+#     '''
+#     View topic page fuction that returns the topic details page and its data 
+#     '''
+#     return render_template('source.html',id = source_id)
