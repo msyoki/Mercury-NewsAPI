6a524ce9a932cf91a88991667345dc00a0d40fac
Msyoki <msyokimutua@gmail.com>
2020-09-13 15:48:47 +0300

    add new article class

---
+++ app/models/article.py
@@ -0,0 +1,14 @@
+class Article:
+    '''
+    Article class that defu=nes an article object
+    '''
+
+    def __init__(self,author,publishedAt,title,urlToImage,description,url)
+        self.author = author
+        self.publishedAt = publishedAt
+        self.title = title
+        self.urlToImage = urlToImage
+        self.description = description
+        self.url = url
+        
+        
