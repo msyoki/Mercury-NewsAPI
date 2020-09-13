4bd2cdf3c7513f37a01bd05bb4a3b586137b77c9
Msyoki <msyokimutua@gmail.com>
2020-09-14 02:11:39 +0300

    edit calling function

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_sources
+from .request import get_sources,get_articles
 #views 
 @app.route('/')
 def index():
@@ -12,12 +12,14 @@ def index():
     # Getting the news sources
     news_sources = get_sources('general')
     tag_line = '"Never miss on the headlines"'
-    return render_template('index.html', title=title, sources=news_sources ,statement = tag_line)
+    return render_template('index.html', title = title, sources = news_sources ,statement = tag_line)
 
 
-# @app.route('/source/<source_id>')  
-# def source(source_id):
-#     '''
-#     View topic page fuction that returns the topic details page and its data 
-#     '''
-#     return render_template('source.html',id = source_id)
+@app.route('/articles/<source_id>')  
+def articles(source_id):
+    '''
+    View function that retuns that renders the source articles
+    '''
+    tag = f'{source_id}'
+    news_articles = get_articles(source_id)
+    return render_template('articles.html',articles = news_articles,tag = tag)
