d46a96edc5370a73e392dca37fe8efe03d9b1272
Msyoki <msyokimutua@gmail.com>
2020-09-14 02:09:37 +0300

    edit article class properties

--- app/models/article.py
+++ app/models/article.py
@@ -3,7 +3,7 @@ class Article:
     Article class that defu=nes an article object
     '''
 
-    def __init__(self,author,publishedAt,title,urlToImage,description,url)
+    def __init__(self,author,publishedAt,title,urlToImage,description,url):
         self.author = author
         self.publishedAt = publishedAt
         self.title = title
