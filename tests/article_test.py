d9407fb6624b51d67ced28b1f08dc6c7f8443746
Msyoki <msyokimutua@gmail.com>
2020-09-15 18:24:48 +0300

    edit tests

--- tests/article_test.py
+++ tests/article_test.py
@@ -11,10 +11,10 @@ class ArticleTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.news_article = Article('BBC News','2020-09-13T19:37:20.5761889Z','Coronavirus: Israeli minister resigns over plans for second lockdown','https://ichef.bbci.co.uk/news/1024/branded_news/068B/production/_114357610_gettyimages-1205060062-2.jpg','Housing Minister Yaacov Litzman says the measure will affect key religious holidays later this month.','http://www.bbc.co.uk/news/world-middle-east-54134869')
+        self.new_article = Article('BBC News','2020-09-13T19:37:20.5761889Z','Coronavirus: Israeli minister resigns over plans for second lockdown','https://ichef.bbci.co.uk/news/1024/branded_news/068B/production/_114357610_gettyimages-1205060062-2.jpg','Housing Minister Yaacov Litzman says the measure will affect key religious holidays later this month.','http://www.bbc.co.uk/news/world-middle-east-54134869')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.news_article,Article))
+        self.assertTrue(isinstance(self.new_article,Article))
 
 
 
@@ -22,12 +22,12 @@ class ArticleTest(unittest.TestCase):
         """
         Test case to check if the Article class is initialised
         """
-        self.assertEqual( self.news_article.author, 'BBC News')
-        self.assertEqual( self.news_article.publishedAt, '2020-09-13T19:37:20.5761889Z')
-        self.assertEqual( self.news_article.title, 'Coronavirus: Israeli minister resigns over plans for second lockdown')
-        self.assertEqual( self.news_article.urlToImage, 'https://ichef.bbci.co.uk/news/1024/branded_news/068B/production/_114357610_gettyimages-1205060062-2.jpg')
-        self.assertEqual( self.news_article.description, 'Housing Minister Yaacov Litzman says the measure will affect key religious holidays later this month.')
-        self.assertEqual( self.news_article.url, 'http://www.bbc.co.uk/news/world-middle-east-54134869')
+        self.assertEqual( self.new_article.author, 'BBC News')
+        self.assertEqual( self.new_article.publishedAt, '2020-09-13T19:37:20.5761889Z')
+        self.assertEqual( self.new_article.title, 'Coronavirus: Israeli minister resigns over plans for second lockdown')
+        self.assertEqual( self.new_article.urlToImage, 'https://ichef.bbci.co.uk/news/1024/branded_news/068B/production/_114357610_gettyimages-1205060062-2.jpg')
+        self.assertEqual( self.new_article.description, 'Housing Minister Yaacov Litzman says the measure will affect key religious holidays later this month.')
+        self.assertEqual( self.new_article.url, 'http://www.bbc.co.uk/news/world-middle-east-54134869')
 
 
 
--- tests/source_test.py
+++ tests/source_test.py
@@ -4,23 +4,23 @@ from app.models import Source
 
 class SourceTest(unittest.TestCase):
     '''
-    Test Class to test the behavioue of the Source class
+    Test Class to test the behaviour of the Source class
     '''
     def setUp(self):
         '''
         setup method that will run before every test
         '''
-        self.news_source = Source('al-jazeera-english','Al Jazeera English','News, analysis from the Middle East and worldwide...','http://www.aljazeera.com','general')
+        self.new_source = Source('al-jazeera-english','Al Jazeera English','News, analysis from the Middle East and worldwide...','http://www.aljazeera.com','general')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.news_source,Source))
+        self.assertTrue(isinstance(self.new_source,Source))
 
     def test_init(self):
         """
         Test case to check if the Source class is initialised
         """
-        self.assertEqual( self.news_source.id, 'al-jazeera-english')
-        self.assertEqual( self.news_source.name, 'Al Jazeera English')
-        self.assertEqual( self.news_source.description, 'News, analysis from the Middle East and worldwide...')
-        self.assertEqual( self.news_source.url, 'http://www.aljazeera.com')
-        self.assertEqual( self.news_source.category, 'general')
+        self.assertEqual( self.new_source.id, 'al-jazeera-english')
+        self.assertEqual( self.new_source.name, 'Al Jazeera English')
+        self.assertEqual( self.new_source.description, 'News, analysis from the Middle East and worldwide...')
+        self.assertEqual( self.new_source.url, 'http://www.aljazeera.com')
+        self.assertEqual( self.new_source.category, 'general')
